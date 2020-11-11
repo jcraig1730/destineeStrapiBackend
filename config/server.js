f6a73eb38ef58e108264f3819b089fca85bff60d
Jared Craig <jcraig1730@gmail.com>
2020-11-11 15:49:31 -0600

    update server url

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "http://167.172.136.197.xip.io",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b970f073b950b708cd0a175eda0bf609'),
+      secret: env("ADMIN_JWT_SECRET", "b970f073b950b708cd0a175eda0bf609"),
     },
   },
 });
