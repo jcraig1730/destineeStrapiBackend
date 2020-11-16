1a15251cc9207057bcf8aedd23377c00d56c32e7
Jared Craig <jcraig1730@gmail.com>
2020-11-16 13:32:00 -0600

    Configure server to use correct urls in production and development

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  // url: "http://167.172.136.197.xip.io:1337",
+  url: process.env.STRAPI_URL || "http://0.0.0.0:1337",
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "b970f073b950b708cd0a175eda0bf609"),
