16f99da1f44a820248de87d1e44e95f587d7e476
Jared Craig <jcraig1730@gmail.com>
2020-11-18 19:11:03 -0600

    Setup stripe for payment processing; add sales order creation logic

--- api/sales-order/controllers/sales-order.js
+++ api/sales-order/controllers/sales-order.js
@@ -1,8 +1,36 @@
-'use strict';
-
+"use strict";
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const stripe = require("stripe")(process.env.STRIPE_API_KEY);
 
-module.exports = {};
+const calculateTotal = (items, shipping = 0) => {
+  const subtotal = items.reduce((total, item) => {
+    return total + item.quantity * item.item.price;
+  }, 0);
+  const tax = subtotal * 0.0825;
+  return Number((tax + subtotal + shipping).toFixed(2)) * 100;
+};
+
+module.exports = {
+  createPaymentIntent: async ({ request, response }) => {
+    console.clear();
+    try {
+      const { items, shipping, total } = request.body;
+      const calculatedTotal = calculateTotal(items, shipping);
+      if (calculatedTotal !== total * 100)
+        throw new Error("Totals do not match");
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: calculatedTotal,
+        currency: "usd",
+      });
+      response.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    } catch (err) {
+      console.log(err);
+      response.send({ err: true });
+    }
+  },
+};
--- api/sales-order/models/sales-order.js
+++ api/sales-order/models/sales-order.js
@@ -1,8 +1,20 @@
-'use strict';
+"use strict";
+
+const { default: createStrapi } = require("strapi");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {
+    afterCreate: async (result, data) => {
+      const cart = await strapi.models.cart
+        .where("id", result.user.cart)
+        .fetch();
+      cart.set("items", []);
+      cart.save();
+    },
+  },
+};
